2e6eec32fddf11beaa8cf453ebd41a644a22a3a4
HyeSu1123 <97337538+HyeSu1123@users.noreply.github.com>
2022-05-13 14:40:14 +0900

    관리자 아이디 비번 틀렸을 때 alert 못띄움..ㅠㅠ

--- src/main/java/com/company/idev/HomeController.java
+++ src/main/java/com/company/idev/HomeController.java
@@ -92,6 +92,7 @@ public class HomeController {
 		
 		session.getAttribute("admin");
 		Members login = mapper.loginAdmin(vo);
+		
 		boolean passMatch;
 		
 		if(login != null) {
@@ -100,19 +101,21 @@ public class HomeController {
 			passMatch = false;
 		}
 		
-		if(login.getAuthority() != 0 && passMatch == true) {
-			session.setAttribute("admin", null);
-			rda.addFlashAttribute("message", "승인 요청중입니다. 현재 이용하실 수 없습니다.");
-			return "redirect:admin.do";
-		}
 		
+		logger.info("관리자 번호 확인" + vo.getAuthority());
 		if(login != null && passMatch == true) {
 			session.setAttribute("admin", login);
 			rda.addFlashAttribute("message", "안녕하세요 관리자님.");
 			return "redirect:/";
-			}else {
+			}
+//		else if(vo.getAuthority()!=0 ) {
+//			session.setAttribute("admin",login);
+//			rda.addFlashAttribute("message", "승인 요청중입니다. 현재 이용하실 수 없습니다.");
+//			return "redirect:admin.do";
+//		}
+		else {
 				session.setAttribute("admin", null);
-				rda.addFlashAttribute("message", "아이디나 비밀번호를 다시 입력해주세요.");
+				rda.addFlashAttribute("message", "승인 요청중입니다. 현재 이용하실 수 없습니다.");
 			}
 			return "redirect:admin.do";
 	}
--- src/main/webapp/WEB-INF/views/admin/AdminLogin.jsp
+++ src/main/webapp/WEB-INF/views/admin/AdminLogin.jsp
@@ -36,7 +36,7 @@
 			<input type="text" name="id" id="id" placeholder="ID를 입력해주세요"><br><br>
 			<label for="password">password</label><br>
 			<input type="password" name="password" id="password" placeholder="password를 입력해주세요"><br><br>
-			<input type="hidden" name="authority" value = "0">
+			<input type="hidden" name="authority" id="authority" value="0"> 
 			<input type="submit" value="로그인"><br>
 			<div>
 			<span>아직 ID가 없으신가요?</span><a href="admin/adminjoin.do">회원가입</a>
