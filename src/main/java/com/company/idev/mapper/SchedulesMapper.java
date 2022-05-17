dbf0356b15a17c23bad85555e1b37157b4dda415
jds0024 <98507150+jds0024@users.noreply.github.com>
2022-05-17 17:36:32 +0900

    Delete src directory

--- src/main/java/com/company/idev/mapper/SchedulesMapper.java
+++
@@ -1,7 +0,0 @@
-package com.company.idev.mapper;
-
-import com.company.idev.dto.Schedules;
-
-public interface SchedulesMapper {
-	void insertSchedule(Schedules vo);
-}
--- src/main/java/com/company/idev/service/AdminService.java
+++
@@ -1,9 +0,0 @@
-package com.company.idev.service;
-
-import com.company.idev.dto.Answer;
-import com.company.idev.dto.Performance;
-
-public interface AdminService {
-	int fileSave(Performance perform);
-	void insertAnswer(Answer answer);
-}
